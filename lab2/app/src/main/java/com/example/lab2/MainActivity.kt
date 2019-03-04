945b348ae12552b72a6ec5347509d710c9034350
Nazeebo <romanov0902@gmail.com>
2019-03-04 05:40:34 +0200

    refactoring

--- lab2/app/src/main/java/com/example/lab2/MainActivity.kt
+++ lab2/app/src/main/java/com/example/lab2/MainActivity.kt
@@ -9,18 +9,18 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    private fun buttonPressed( button : Button ): View.OnClickListener = View.OnClickListener() { view ->
+    private fun buttonPressed( button : String ): View.OnClickListener = View.OnClickListener() { view ->
         if(firstNum.text.isEmpty() or  secondNum.text.isEmpty())
             Snackbar.make(view, "Заполните оба числа", Snackbar.LENGTH_LONG).show()
         else {
             val first = firstNum.text.toString().toInt()
             val second = secondNum.text.toString().toInt()
-            when(button.text.toString()){
-                "+" ->  text.text = (first + second).toString()
-                "-" ->  text.text = (first - second).toString()
-                "*" ->  text.text = (first * second).toString()
-                "/" ->  if(second != 0) text.text = (first / second).toString()
-                    else text.text = "inf"
+            when(button){
+                "+" ->  edit.text = (first + second).toString()
+                "-" ->  edit.text = (first - second).toString()
+                "*" ->  edit.text = (first * second).toString()
+                "/" ->  if(second != 0) edit.text = (first / second).toString()
+                    else edit.text = "inf"
             }
         }
     }
@@ -29,9 +29,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        b1.setOnClickListener(buttonPressed(b1))
-        b2.setOnClickListener(buttonPressed(b2))
-        b3.setOnClickListener(buttonPressed(b3))
-        b4.setOnClickListener(buttonPressed(b4))
+        b1.setOnClickListener(buttonPressed(b1.text.toString()))
+        b2.setOnClickListener(buttonPressed(b2.text.toString()))
+        b3.setOnClickListener(buttonPressed(b3.text.toString()))
+        b4.setOnClickListener(buttonPressed(b4.text.toString()))
     }
 }
